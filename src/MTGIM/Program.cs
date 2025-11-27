81356b12de4d26acee73262c0a893ab2bd917307
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-11-27 14:47:09 +0100

    Add persistence project and DB configuration using appsettings.json (#24)

--- src/MTGIM/Program.cs
+++ src/MTGIM/Program.cs
@@ -1,8 +1,13 @@
-using System;
-using System.Linq;
+using MGTIM.Infrastructure.Persistence;
+using MGTIM.Infrastructure.Persistence.Entities;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MTGIM.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DbOptions>(
+    builder.Configuration.GetSection(DbOptions.SectionName));
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    (serviceProvider, options) =>
+    {
+        var dbOptions = serviceProvider.GetRequiredService<IOptions<DbOptions>>().Value;
+        
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("MTGIM"),
+            sqlOptions =>
+            {
+                sqlOptions.CommandTimeout(dbOptions.CommandTimeoutInSeconds);
+            });
+
+        options.EnableDetailedErrors(dbOptions.EnableDetailedErrors);
+        options.EnableSensitiveDataLogging(dbOptions.EnableSensitiveDataLogging);
+    })
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
